93d74297f9f5c5f33ed8f10cf7c9ffd3f2d13716
Will Hedgecock <will.hedgecock@gmail.com>
2024-03-26 17:00:34 -0500

    Updates

--- apps/simple_flowgraph.py
+++ apps/simple_flowgraph.py
@@ -39,7 +39,7 @@ def time_it(tb):
       tb.run()
       stop = time.time_ns()
       np_times[r] = (stop-start)/1e9
-      print("%4d, %10d, %20.10f" % (tb.run_num, r, np_times[r]))
+      print("%4d - %10d: Work Time: %20.10f, Clock Time: %20.10f" % (tb.run_num, r, tb.head.pc_work_time_avg(), np_times[r]))
 
    avg_time = np.mean(np_times)
    var_time = np.var(np_times)
